f0259cd9cd0f9f7137061b9b3a5d3f9d60a83a64
Priya Mahato <209283042+priyamahato86@users.noreply.github.com>
2025-07-15 22:14:00 +0530

    Create 3136Valid Word.java

---
+++ 3136Valid Word.java
@@ -0,0 +1,41 @@
+// A word is considered valid if:
+
+// It contains a minimum of 3 characters.
+// It contains only digits (0-9), and English letters (uppercase and lowercase).
+// It includes at least one vowel.
+// It includes at least one consonant.
+// You are given a string word.
+
+// Return true if word is valid, otherwise, return false.
+
+// Notes:
+
+// 'a', 'e', 'i', 'o', 'u', and their uppercases are vowels.
+// A consonant is an English letter that is not a vowel.
+
+##CODE:
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                ch = Character.toLowerCase(ch);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
