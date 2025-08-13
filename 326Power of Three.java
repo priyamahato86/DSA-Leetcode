5296b23805f397b5e40414e593ead55090a6d719
Priya Mahato <209283042+priyamahato86@users.noreply.github.com>
2025-08-13 23:02:47 +0530

    Create 326Power of Three.java

---
+++ 326Power of Three.java
@@ -0,0 +1,17 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+##CODE:
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        double logValue = Math.log10(n) / Math.log10(3);
+
+        // Check if logValue is an integer
+        return logValue == (int)logValue;
+    }
+}
