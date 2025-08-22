dd082cda635664ed3ba4fd8d26311a00053b4765
Priya Mahato <209283042+priyamahato86@users.noreply.github.com>
2025-08-22 23:15:36 +0530

    Create 3195Find the Minimum Area to Cover All Ones I.java

---
+++ 3195Find the Minimum Area to Cover All Ones I.java
@@ -0,0 +1,30 @@
+// You are given a 2D binary array grid. Find a rectangle with horizontal and vertical sides with the smallest area, such that all the 1's in grid lie inside this rectangle.
+
+// Return the minimum possible area of the rectangle.
+
+##CODE:
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int m = grid.length;          // Rows
+        int n = grid[0].length;       // Columns
+
+        int minRow = m;
+        int maxRow = -1;
+        int minCol = n;
+        int maxCol = -1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    minRow = Math.min(minRow, i);
+                    maxRow = Math.max(maxRow, i);
+
+                    minCol = Math.min(minCol, j);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+}
