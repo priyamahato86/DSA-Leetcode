5e279b260a040c9dba4b4e913c5be84cefa5725d
Priya Mahato <209283042+priyamahato86@users.noreply.github.com>
2025-09-04 22:38:22 +0530

    Create 3516Find Closest Person.java

---
+++ 3516Find Closest Person.java
@@ -0,0 +1,31 @@
+// You are given three integers x, y, and z, representing the positions of three people on a number line:
+
+// x is the position of Person 1.
+// y is the position of Person 2.
+// z is the position of Person 3, who does not move.
+// Both Person 1 and Person 2 move toward Person 3 at the same speed.
+
+// Determine which person reaches Person 3 first:
+
+// Return 1 if Person 1 arrives first.
+// Return 2 if Person 2 arrives first.
+// Return 0 if both arrive at the same time.
+// Return the result accordingly.
+
+##CODE:
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int xSteps = Math.abs(z - x);
+        int ySteps = Math.abs(z - y);
+
+        if (xSteps == ySteps) {
+            return 0;
+        }
+
+        if (xSteps < ySteps) {
+            return 1;
+        }
+
+        return 2;
+    }
+}
