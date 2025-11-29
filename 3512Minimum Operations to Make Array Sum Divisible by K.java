d20a4cae90c7228d30b731ae293abe008d57c903
Priya Mahato <209283042+priyamahato86@users.noreply.github.com>
2025-11-29 22:01:02 +0530

    Create 3512Minimum Operations to Make Array Sum Divisible by K.java

---
+++ 3512Minimum Operations to Make Array Sum Divisible by K.java
@@ -0,0 +1,15 @@
+// You are given an integer array nums and an integer k. You can perform the following operation any number of times:
+
+// Select an index i and replace nums[i] with nums[i] - 1.
+// Return the minimum number of operations required to make the sum of the array divisible by k.
+
+##CODE:
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int sum = Arrays.stream(nums).sum();
+
+        int rem = sum % k;
+
+        return rem;
+    }
+}
