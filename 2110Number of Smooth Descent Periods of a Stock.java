5664e2db71f60b2fcf9cb2a13fce732f5b3a3973
Priya Mahato <209283042+priyamahato86@users.noreply.github.com>
2025-12-15 21:50:00 +0530

    Create 2110Number of Smooth Descent Periods of a Stock.java

---
+++ 2110Number of Smooth Descent Periods of a Stock.java
@@ -0,0 +1,24 @@
+// You are given an integer array prices representing the daily price history of a stock, where prices[i] is the stock price on the ith day.
+
+// A smooth descent period of a stock consists of one or more contiguous days such that the price on each day is lower than the price on the preceding day by exactly 1. The first day of the period is exempted from this rule.
+
+// Return the number of smooth descent periods.
+
+##CODE:
+
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long result = 1;
+        long count = 1;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i - 1] - prices[i] == 1) {
+                count++;
+            } else {
+                count = 1;
+            }
+            result += count;
+        }
+        return result;
+    }
+}
